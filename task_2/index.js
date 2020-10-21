49ad2ba7023d7aec250a101043173d2cd9d3b2d5
akups <appiah-lindberg@outlook.com>
2020-10-21 07:41:06 +0200

    removed solution

--- task_2/index.js
+++ task_2/index.js
@@ -6,43 +6,25 @@ app.use(express.json());
 
 // Your code starts here. Placeholders for .get and .post are provided for
 //  your convenience.
-const candidates = [];
 
-app.post("/candidates", function (req, res) {
-  const candidate = req.body;
+app.post("/candidates", function (req, res) {});
+/* 
 
-  candidates.push(candidate);
-
-  res.sendStatus(200);
-});
-
-function calcScore(skillCan, skillsNeeded) {
-  let score = 0;
-  skillsNeeded.forEach((skill) => {
-    if (skillCan.findIndex((sk) => sk == skill) > -1) {
-      score = score + 1;
-    }
-  });
-  return score;
+{
+    name: "Akua",
+    lastname: "Appiah",
+    skills: ["nodejs","reactjs","mongodb"]
 }
 
-app.get("/candidates/search", function (req, res) {
-  if (candidates.length == 0) res.sendStatus(404);
-  const { skills: skillsText } = req.query;
+{
+    name: "Bogdan",
+    lastname: "Nedelcu",
+    skills: ["nodejs","express","sql"]
+}
 
-  const skillsNeeded = skillsText.split(",");
+*/
 
-  const scoredList = candidates
-    .map((cand) => ({
-      ...cand,
-      score: calcScore(cand.skills, skillsNeeded),
-    }))
-    .sort((a, b) => b.score - a.score)
-    .filter((cand) => cand.score >= 1);
-  if (scoredList.length === 0) {
-    res.send(404);
-  }
-  res.json(scoredList[0]);
-});
+// /candidates/search?skills=javascript,nodejs,express
+app.get("/candidates/search", function (req, res) {});
 
 app.listen(process.env.HTTP_PORT || 3000, () => console.log("running"));
